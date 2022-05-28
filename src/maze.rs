1cbc09ff53aefa084c872afa0ffd24c9373e3c68
dongRogen <3601778801@qq.com>
2022-05-28 19:22:40 +0800

    修复生成中断的问题 **调整 v2, maze** - v2   - 调整 Pos 的结构为 x, y 便于阅读     - 调整结构体创建   - 调整 Pos.cp 调用方式   - 调整坐标移动（Pos.mv）实现 - maze   - 删除 Mark::new —— 基本上用不到   - 补完 Maze.gen   - 将用到 Maze.map 的函数都移入 Maze 实现   - 简化 Maze.dig 实现   - 补充 Maze.get_mark 中下标越界的场景处理   - 重命名 Maze.dig_to 为 conn，并简化逻辑

--- src/maze.rs
+++ src/maze.rs
@@ -2,20 +2,25 @@
 use crate::v2::{rev, Dir, Pos, D, L, R, U};
 use rand::{self, Rng};
 
-/// limit
+// --- 常量
+// 限制
 const W_MIN: u32 = 3;
 const H_MIN: u32 = 3;
 const W_MAX: u32 = i32::MAX as u32;
 const H_MAX: u32 = i32::MAX as u32;
+
+// 支持 迭代
+/// 方向枚举集合
 const DIR_LS: [Dir; 4] = [Dir::Up, Dir::Down, Dir::Left, Dir::Right];
 
-/// pass dir at position
+// --- 结构
+/// 点位标记
 pub struct Mark {
     p: Pos,
     val: u8,
 }
 
-/// maze
+/// 迷宫
 pub struct Maze {
     w: u32,
     h: u32,
@@ -24,115 +29,139 @@ pub struct Maze {
 
 // --- impl
 
-impl Mark {
-    pub fn new() -> Mark {
-        Mark {
-            p: Pos::at(0, 0),
-            val: 0,
-        }
-    }
-}
-
 impl Maze {
     pub fn gen(w: u32, h: u32) -> Result<Maze, String> {
         if w < W_MIN || w > W_MAX || h < H_MIN || h > H_MAX {
             return Err("范围不合适".to_string());
         }
         println!("gen start({} * {})", w, h);
-        // init map
+        // 初始化
         let mut maze = Maze {
             w,
             h,
-            map: vec![vec![0; h as usize]; w as usize],
+            map: vec![vec![0u8; h as usize]; w as usize],
         };
-        // dig
-        dig(&mut maze);
+        // 开通
+        let succ = maze.dig();
 
-        println!("finish");
-        return Ok(maze);
-    }
-}
-
-/// 挖掘通道
-fn dig(maze: &mut Maze) {
-    let mut pre_d = 0u8;
-    // 剩余数量
-    let mut more = maze.w * maze.h;
-    // 点位表
-    let map = &mut maze.map;
-    // random number generator
-    let mut rng = rand::thread_rng();
-    // 拐点
-    let mut corners: Vec<Pos> = Vec::with_capacity(more as usize);
-    // start pos
-    let mut p = Pos::at(rng.gen_range(0..maze.w), rng.gen_range(0..maze.h));
-    println!("start at ({},{})", p.0, p.1);
-
-    let count = maze.w * maze.h;
-    while more > 0 {
-        let tc = count - more;
-        print!("\r{}({}%)", tc, tc / count * 100);
-        let next_d: u8;
-        let opt_len: usize;
-        // 四周情况
-        let opt_d = nearby(&p, &map, true);
-        opt_len = opt_d.len();
-        // 如果没有可用方向，回溯至上一个拐点
-        if opt_len < 1 {
-            if let Some(bp) = corners.pop() {
-                println!("\rback");
-                p = bp;
-                continue;
-            } else {
-                println!("\nno corners");
-                break;// 没有拐点，结束
-            }
-        }
-        // 取方向 + 坐标
-        let tmp = if opt_len < 2 {
-            opt_d.get(0)
+        // return
+        if succ {
+            Ok(maze)
         } else {
-            opt_d.get(rng.gen_range(0..opt_len))
+            Err("生成失败，请检查".to_string())
         }
-        .unwrap();
-        next_d = tmp.val; // val here is dir-val
-        p.goto(&tmp.p);
-        dig_to(&p, rev(pre_d) + next_d, map);
-        // mark turning
-        if pre_d > 0 && next_d != pre_d {
-            corners.push(Pos::cp(&p));
-        }
-        pre_d = next_d;
-        more -= 1;
     }
-    println!("end dig, more={}", more);
-}
 
-/// 获取四周的情况
-fn nearby(p: &Pos, map: &Vec<Vec<u8>>, just_unk: bool) -> Vec<Mark> {
-    let mut v: Vec<Mark> = Vec::with_capacity(4);
-    for d in DIR_LS {
-        if let Some(n) = p.peek(&d) {
-            let m = get_mark(p, map);
-            if m > 0 && just_unk {
-                // println!("\r({},{})={}", n.0, n.1, m);
-                continue;
+    /// 挖掘通道
+    fn dig(&mut self) -> bool {
+        let mut pre_d = 0u8;
+        // 剩余数量
+        let mut more = self.w * self.h;
+        // 随机数生成器
+        let mut rng = rand::thread_rng();
+        // 拐点
+        let mut corners: Vec<Pos> = Vec::with_capacity(more as usize);
+        // 坐标 —— 首个随机
+        let mut p = Pos {
+            x: rng.gen_range(0..self.w),
+            y: rng.gen_range(0..self.h),
+        };
+        println!("start at ({},{})", p.x, p.y);
+
+        let all = self.w * self.h;
+        while more > 0 {
+            let count = all - more;
+            print!(
+                "\r {}({}%) ",
+                count,
+                ((count as f32 / all as f32) * 100.0) as u32
+            );
+            // 四周情况
+            let opt_d: Vec<Mark> = self.nearby(&p, true);
+            let opt_len = opt_d.len();
+
+            // 如果没有可用方向，回溯至上一个拐点
+            if opt_len < 1 {
+                if let Some(next) = corners.pop() {
+                    // println!("\rback");
+                    p = next;
+                    continue;
+                }
+                // 没有拐点，结束
+                println!("... 100%");
+                more -= 1;
+                break;
             }
-            v.push(Mark { p: n, val: d.val() });
+
+            // 取方向 + 坐标
+            let dest: &Mark = &opt_d[if opt_len < 2 {
+                0
+            } else {
+                rng.gen_range(0..opt_len)
+            }];
+
+            // dest.val 表示其相对于 p 的方向
+            self.dig_to(&p, dest);
+
+            // 保存拐点
+            let next_d = dest.val;
+            if pre_d > 0 && next_d != pre_d {
+                corners.push(Pos::cp(&p));
+            }
+
+            // 准备下一轮
+            p.goto(&dest.p);
+            pre_d = next_d;
+            more -= 1;
+        }
+        println!("end dig");
+        return more < 1;
+    }
+
+    /// # 连接两点
+    /// **dest**中**val**的值是相对于**src**的方向
+    fn dig_to(&mut self, src: &Pos, dest: &Mark) {
+        let (sx, sy, dx, dy) = (
+            src.x as usize,
+            src.y as usize,
+            dest.p.x as usize,
+            dest.p.y as usize,
+        );
+        // update src
+        conn(&mut self.map[sx][sy], dest.val);
+        // udpate dest
+        conn(&mut self.map[dx][dy], rev(dest.val));
+    }
+
+    /// 获取四周的情况
+    fn nearby(&self, p: &Pos, just_unk: bool) -> Vec<Mark> {
+        let mut v: Vec<Mark> = Vec::with_capacity(4);
+        for d in DIR_LS {
+            if let Some(n) = p.peek(&d) {
+                if let Some(m) = self.get_mark(&n) {
+                    if m > 0 && just_unk {
+                        continue;
+                    }
+                    v.push(Mark { p: n, val: d.val() });
+                }
+            }
+        }
+        return v;
+    }
+
+    fn get_mark(&self, p: &Pos) -> Option<u8> {
+        if p.x >= self.w || p.y >= self.h {
+            None
+        } else {
+            Some(self.map[p.x as usize][p.y as usize])
         }
     }
-    return v;
 }
 
-fn get_mark(p: &Pos, map: &Vec<Vec<u8>>) -> u8 {
-    let v = &map[p.0 as usize];
-    return *(&v[p.1 as usize]);
-}
-
-fn dig_to(p: &Pos, mark: u8, map: &mut Vec<Vec<u8>>) {
-    let v = &mut map[p.0 as usize];
-    let y = p.1 as usize;
-    if v[y] < U + D + L + R {
-        v[y] = mark;
+/// # 尝试连接
+/// 若**src**已经4向通，则不更新
+fn conn(src: &mut u8, dest: u8) {
+    if *src < U + D + L + R {
+        *src += dest;
     }
 }
--- src/v2.rs
+++ src/v2.rs
@@ -1,11 +1,11 @@
 
-/// directory codes
+// 方向真值
 pub const U: u8 = 1;
 pub const D: u8 = 2;
 pub const L: u8 = 4;
 pub const R: u8 = 8;
 
-/// 反向
+/// 反向真值
 pub fn rev(d: u8) -> u8 {
     match d {
         U | L => d * 2,
@@ -14,10 +14,13 @@ pub fn rev(d: u8) -> u8 {
     }
 }
 
-/// position
-pub struct Pos(pub u32, pub u32);
+/// 无符号坐标
+pub struct Pos {
+    pub x: u32,
+    pub y: u32,
+}
 
-/// directory
+/// 方向枚举
 pub enum Dir {
     Up,
     Down,
@@ -25,7 +28,7 @@ pub enum Dir {
     Right,
 }
 
-/// link(cross) case
+/// 连接方向枚举
 pub enum Crs {
     // U-1     D-2     UD-3
     Ver,
@@ -49,73 +52,67 @@ pub enum Crs {
 
 impl Pos {
     pub fn at(x: u32, y: u32) -> Pos {
-        Pos(x, y)
+        Pos { x, y }
     }
 
-    pub fn cp(p: &Pos) -> Pos {
-        Pos(p.0, p.1)
+    /// 复制坐标
+    pub fn cp(&self) -> Pos {
+        Pos {
+            x: self.x,
+            y: self.y,
+        }
     }
 
     /// 按方向单步移动坐标。
     /// 返回成功与否
     /// 不成功不移动
     pub fn mv(&mut self, d: &Dir) -> bool {
-        let mut x = self.0;
-        let mut y = self.1;
-        let mut r = true;
+        let max = i32::MAX as u32;
+        if self.x > max {
+            self.x = max;
+            return false;
+        }
+        if self.y > max {
+            self.y = max;
+            return false;
+        }
+        let mut x = self.x as i32;
+        let mut y = self.y as i32;
         match d {
-            Dir::Up => {
-                if y < 1 {
-                    r = false;
-                } else {
-                    y -= 1;
-                }
-            }
-            Dir::Down => {
-                if y > i32::MAX as u32 {
-                    r = false;
-                } else {
-                    y += 1;
-                }
-            }
-            Dir::Left => {
-                if x < 1 {
-                    r = false;
-                } else {
-                    x -= 1;
-                }
-            }
-            Dir::Right => {
-                if x > i32::MAX as u32 {
-                    r = false;
-                } else {
-                    x += 1;
-                }
-            }
+            Dir::Up => y -= 1,
+            Dir::Down => y += 1,
+            Dir::Left => x -= 1,
+            Dir::Right => x += 1,
         }
-        if r {
-            self.0 = x;
-            self.1 = y;
+        // return
+        if x < 0 || y < 0 {
+            false
+        } else {
+            self.x = x as u32;
+            self.y = y as u32;
+            true
         }
-        return r;
     }
 
     /// 尝试根据方向获取新坐标
     pub fn peek(&self, d: &Dir) -> Option<Pos> {
-        let mut tmp = Pos::cp(self);
-        let f = tmp.mv(d);
-        return if f { Some(tmp) } else { None };
+        let mut tmp = self.cp();
+        // return
+        if tmp.mv(d) {
+            Some(tmp)
+        } else {
+            None
+        }
     }
 
     /// 跳转坐标
     pub fn goto(&mut self, p: &Pos) {
-        self.0 = p.0;
-        self.1 = p.1;
+        self.x = p.x;
+        self.y = p.y;
     }
 }
 
 impl Dir {
-    /// by code
     pub fn by(v: u8) -> Option<Dir> {
         match v {
             U => Some(Dir::Up),
@@ -126,7 +123,6 @@ impl Dir {
         }
     }
 
-    /// get code
     pub fn val(&self) -> u8 {
         match self {
             Dir::Up => U,
@@ -136,8 +132,8 @@ impl Dir {
         }
     }
 
-    /// get reverse dir
-    pub fn rev(self) -> Dir {
+    /// 取反方向枚举
+    pub fn rev(&self) -> Dir {
         match self {
             Dir::Up => Dir::Down,
             Dir::Down => Dir::Up,
