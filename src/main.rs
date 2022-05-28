e9f3cc91df52fa2f0938eaf21437f5058919d5c4
dongRogen <3601778801@qq.com>
2022-05-28 19:50:49 +0800

    长宽从启动参数中取

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use maze::Maze;
-
+use std::env;
 
 pub mod maze;
 pub mod v2;
@@ -9,6 +9,17 @@ fn main() {
 }
 
 fn run() {
-    let _ = Maze::gen(10, 10);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("usage: maze <width> <height>");
+        return;
+    }
+    if let Ok(w) = args[1].parse::<u32>() {
+        if let Ok(h) = args[2].parse::<u32>() {
+            if let Err(msg) = Maze::gen(w, h) {
+                println!("gen maze failure! msg:\n{}", msg)
+            }
+        }
+    }
 }
 
