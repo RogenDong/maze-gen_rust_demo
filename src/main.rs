ae23189d61caf866cc1d9da58cef322a23cd6711
dongRogen <3601778801@qq.com>
2022-06-02 00:59:50 +0800

    - 添加打印迷宫的开关

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,20 @@ fn main() {
 fn run() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        println!("usage: maze <width> <height>");
+        println!("usage: maze <width> <height> [print? default:true]");
         return;
     }
 
+    let mut prt = true;
     let mut res: Result<Maze, String> = Err("invalid width or height!".to_string());
     if let Ok(w) = args[1].parse::<u32>() {
         if let Ok(h) = args[2].parse::<u32>() {
             res = Maze::gen(w, h);
+            if args.len() > 3 {
+                if let Ok(pt) = args[3].parse::<bool>() {
+                    prt = pt;
+                }
+            }
         }
     }
 
@@ -31,15 +37,17 @@ fn run() {
         }
     }
 
-    // let conn_char: Vec<char> = " ↓↑│→┘┐┤←└┌├─┴┬┼".chars().collect();
-    let conn_char: Vec<char> = " ··│·┘┐┤·└┌├─┴┬┼".chars().collect();
-    println!();
-    for row in maze.map {
-        for val in row {
-            print!("{}", conn_char[val as usize]);
-            // print!("{}", val as usize);
+    if prt {
+        // let conn_char: Vec<char> = " ↓↑│→┘┐┤←└┌├─┴┬┼".chars().collect();
+        let conn_char: Vec<char> = " ··│·┘┐┤·└┌├─┴┬┼".chars().collect();
+        println!();
+        for row in maze.map {
+            for val in row {
+                print!("{}", conn_char[val as usize]);
+                // print!("{}", val as usize);
+            }
+            println!();
         }
         println!();
     }
-    println!();
 }
